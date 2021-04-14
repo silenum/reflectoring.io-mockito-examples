ed2a55cef51a784ee8b5efc4572e65d72e7b929d
Lukas Leuenberger <lukas.leuenberger@hotmail.com>
2021-04-14 17:24:46 +0200

    Initial Commit

---
+++ src/test/java/org/example/silenum/mockito/business/service/ExampleDontRecycleMocksResolution.java
@@ -0,0 +1,50 @@
+package org.example.silenum.mockito.business.service;
+
+import org.example.silenum.mockito.domain.repository.CityRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class ExampleDontRecycleMocksResolution {
+
+    private CityService cityService;
+
+    @Mock
+    private CityRepository cityRepository;
+
+    void initializeScenarioOne() {
+        // Mockito behaviour declarations
+    }
+
+    void initializeScenarioTwo() {
+        // Mockito behaviour declarations
+    }
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        cityService = new CityServiceImpl(cityRepository);
+    }
+
+    @Test
+    void testOne() {
+        initializeScenarioOne();
+        // Test Case One
+    }
+
+    @Test
+    void testTwo() {
+        initializeScenarioTwo();
+        // Test Case Two
+    }
+
+    @Test
+    void testThree() {
+        initializeScenarioOne();
+        // ...
+        initializeScenarioTwo();
+        // ...
+        // Another Test Case
+    }
+}
